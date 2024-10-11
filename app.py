92948aac9ccf1d626b5d22ab9079b59dcd1117c6
Khen Sorela <khensorela27@gmail.com>
2024-10-11 22:00:36 +0800

    Fixed app.py to resolve eventlet issue and improve code structure

--- app.py
+++ app.py
@@ -1,58 +1,70 @@
-from flask import Flask,render_template,request
+import eventlet
+eventlet.monkey_patch()  # Add this to enable eventlet monkey patching
+
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 import random
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='eventlet')  # Explicitly set eventlet as async mode
 
-# python dict. Store connected users. Key is socket id, value is username and avatarUrl 
+# Store connected users. Key is socket id, value is username and avatarUrl
 users = {}
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# we're listening for the connect event
+# We're listening for the connect event
 @socketio.on("connect")
 def handle_connect():
     username = f"User_{random.randint(1000,9999)}"
-    gender = random.choice(["girl","boy"])
-    # https://avatar.iran.liara.run/public/boy?username=User_123
-    avatar_url = f" https://avatar.iran.liara.run/public/{gender}?username={username}"
+    gender = random.choice(["girl", "boy"])
+    avatar_url = f"https://avatar.iran.liara.run/public/{gender}?username={username}"  # Fixed the extra space in the URL
 
-    users[request.sid] = { "username":username,"avatar":avatar_url}
+    users[request.sid] = {"username": username, "avatar": avatar_url}
 
-    emit("user_joined", {"username":username,"avatar":avatar_url},broadcast=True)
+    # Notify other users that someone joined
+    emit("user_joined", {"username": username, "avatar": avatar_url}, broadcast=True)
 
-    emit("set_username", {"username":username})
+    # Send the new user their username
+    emit("set_username", {"username": username})
 
 @socketio.on("disconnect")
 def handle_disconnect():
     user = users.pop(request.sid, None)
     if user:
-      emit("user_left", {"username":user["username"]},broadcast=True)
-
+        # Notify others when a user leaves
+        emit("user_left", {"username": user["username"]}, broadcast=True)
 
 @socketio.on("send_message")
 def handle_message(data):
     user = users.get(request.sid)
     if user:
-        emit("new_message", {
-            "username":user["username"],
-            "avatar":user["avatar"],
-            "message":data["message"]
-        }, broadcast=True)
+        message = data.get("message", "")
+        if message:  # Ensure there's a message to send
+            emit(
+                "new_message",
+                {
+                    "username": user["username"],
+                    "avatar": user["avatar"],
+                    "message": message,
+                },
+                broadcast=True,
+            )
 
 @socketio.on("update_username")
 def handle_update_username(data):
     old_username = users[request.sid]["username"]
-    new_username = data["username"]
+    new_username = data.get("username", old_username)  # Default to old username if none provided
     users[request.sid]["username"] = new_username
 
-    emit("username_updated", {
-        "old_username":old_username,
-        "new_username":new_username
-    }, broadcast=True)
+    # Notify others of the username change
+    emit(
+        "username_updated",
+        {"old_username": old_username, "new_username": new_username},
+        broadcast=True,
+    )
 
 if __name__ == "__main__":
-    socketio.run(app) 
+    socketio.run(app)
